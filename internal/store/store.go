8239920363ba3c11a9a564370b3f79fdfe100d4a
ethan-stone <ethan.stone9352@gmail.com>
2025-03-21 12:55:01 -0400

    add basic op log

--- internal/store/store.go
+++ internal/store/store.go
@@ -1,16 +1,75 @@
 package store
 
-import "sync"
+import (
+	"log"
+	"sync"
+)
 
 type KeyValueStore struct {
 	sync.RWMutex
 	data map[string]string
 }
 
+type OpTypeEnum int
+
+const (
+	Get OpTypeEnum = iota
+	Put
+	Delete
+)
+
+type OpLogEntry struct {
+	OpType OpTypeEnum
+	Key    string
+	Val    *string // Delete entries will not have the value
+}
+
+type OpLogEntries struct {
+	sync.RWMutex
+	entries []*OpLogEntry
+}
+
+func (opLog *OpLogEntries) AddEntry(entry *OpLogEntry) {
+	opLog.Lock()
+	defer opLog.Unlock()
+	opLog.entries = append(opLog.entries, entry)
+
+	var opTypeString string
+
+	if entry.OpType == Get {
+		opTypeString = "GET"
+	} else if entry.OpType == Put {
+		opTypeString = "PUT"
+	} else {
+		opTypeString = "DELETE"
+	}
+
+	if entry.OpType == Delete {
+		log.Printf("%s %s", opTypeString, entry.Key)
+	} else {
+		log.Printf("%s %s %s", opTypeString, entry.Key, *entry.Val)
+	}
+
+}
+
+var OpLog = &OpLogEntries{
+	entries: make([]*OpLogEntry, 0),
+}
+
 func (store *KeyValueStore) Get(key string) (string, bool) {
 	store.RLock()
 	defer store.RUnlock()
 	val, ok := store.data[key]
+
+	// not finding a key is considered a no op
+	if ok {
+		defer OpLog.AddEntry(&OpLogEntry{
+			OpType: Get,
+			Key:    key,
+			Val:    &val,
+		})
+	}
+
 	return val, ok
 }
 
@@ -18,11 +77,22 @@ func (store *KeyValueStore) Put(key string, val string) {
 	store.Lock()
 	defer store.Unlock()
 	store.data[key] = val
+
+	defer OpLog.AddEntry(&OpLogEntry{
+		OpType: Put,
+		Key:    key,
+		Val:    &val,
+	})
 }
 
 func (store *KeyValueStore) Del(key string) {
 	store.Lock()
 	defer store.Unlock()
+	defer OpLog.AddEntry(&OpLogEntry{
+		OpType: Delete,
+		Key:    key,
+		Val:    nil,
+	})
 	delete(store.data, key)
 }
 
