9f25c943e251f040e97101889e8e25647036d6e7
ethan-stone <ethan.stone9352@gmail.com>
2025-03-24 23:31:45 -0400

    stub test file

--- internal/store/factory_test.go
+++ internal/store/factory_test.go
@@ -1,10 +1,18 @@
 package store
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/ethan-stone/go-key-store/internal/rpc"
+)
 
 type MockRpcClientManager struct {
+	MockGetOrCreateRpcClient func(config *rpc.RpcClientConfig) (rpc.RpcClient, error)
 }
 
-func TestGetStore(t *testing.T) {
+func (m *MockRpcClientManager) GetOrCreateRpcClient(config *rpc.RpcClientConfig) (rpc.RpcClient, error) {
+	return m.MockGetOrCreateRpcClient(config)
+}
 
+func TestReturnsRemoteStore(t *testing.T) {
 }
